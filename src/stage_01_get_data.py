b2ee3b426389b13f2f4f98e23e045c6c0a73f750
Gokulkrishn <K.Gokulkrishn@gmail.com>
2023-03-05 05:31:37 +0530

    stage 01 code

--- src/stage_01_get_data.py
+++ src/stage_01_get_data.py
@@ -1,13 +1,11 @@
 import argparse
 import os
-import shutil
-from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
-import random
+import urllib.request as req
 
 
-STAGE = "STAGE_NAME" ## <<< change stage name 
+STAGE = "stage 01 get data" ## <<< change stage name 
 
 logging.basicConfig(
     filename=os.path.join("logs", 'running_logs.log'), 
@@ -17,23 +15,35 @@ logging.basicConfig(
     )
 
 
-def main(config_path, params_path):
+def main(config_path):
     ## read config files
     config = read_yaml(config_path)
-    params = read_yaml(params_path)
-    pass
+    source_data_url = config["source_data_url"]
+
+    local_data_dir = config["source_download_dirs"]["data_dir"]
+    create_directories([local_data_dir])
+
+    data_filename = config["source_download_dirs"]["data_file"]
+    local_data_filepath = os.path.join(local_data_dir,data_filename)
+   
+    logging.info(f'File download started')
+    filename,headers = req.urlretrieve(source_data_url,local_data_filepath)
+
+    logging.info(f'File download completed')
+    logging.info(f'File is present at {local_data_filepath}')
+    logging.info(f"header are {headers}")
+
 
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default="configs/config.yaml")
-    args.add_argument("--params", "-p", default="params.yaml")
     parsed_args = args.parse_args()
 
     try:
         logging.info("\n********************")
         logging.info(f">>>>> stage {STAGE} started <<<<<")
-        main(config_path=parsed_args.config, params_path=parsed_args.params)
+        main(config_path=parsed_args.config)
         logging.info(f">>>>> stage {STAGE} completed!<<<<<\n")
     except Exception as e:
         logging.exception(e)
